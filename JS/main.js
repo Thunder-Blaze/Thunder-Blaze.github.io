e455090b069125bb287b5de8da031c67d31ae9a6
ThunderBlaze <darkthunderstormshiva@gmail.com>
2024-09-17 00:50:38 +0530

    New Files Added

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+let originalString = "Using JS, you can change the color of a webpage's background, text, and elements.";
+
+let newString = originalString
+    .replace("color", "colour")
+    .replace("JS", `JavaScript is
+dff`);
+
+console.log(newString);
+
+
+// <center><img src="https://cdn.discordapp.com/attachments/1136989514653519924/1159864589555617822/gintokisakata.png?ex=65329313&amp;is=65201e13&amp;hm=cf9d9816895b58db04b0709ffc01a6ee9588e28d9335d6d4bfaeffe303e9ce5b&amp;" width="100%">
+// <br>
+// <img src="https://cdn.discordapp.com/attachments/1136989445992751264/1148145538617856070/asdasdaUntitledb-1.png" width="100%">
+// <h1><strong>Happy Birthday <a href="https://anilist.co/character/672/Gintoki-Sakata" rel="noopener noreferrer" target="_blank">Gintoki Sakata!</a></strong></h1>
+// <strong>Birthday : October 10</strong>
+// Gintoki Sakata (坂田 銀時, Sakata Gintoki?) is the main protagonist of the Gintama series. He is the founder and president of the Yorozuya, as well as a highly-skilled samurai, having fought in the Joui War in the past. During the war, he became known as the Shiroyasha (literally meaning "White Yaksha", Yaksha being a spirit or demon in Buddhism, Hinduism and Jainism) due to his powerful swordsmanship, demonic appearance, white clothing, and silver hair.
+// <img src="https://cdn.discordapp.com/attachments/1136989445992751264/1148145538617856070/asdasdaUntitledb-1.png" width="100%">
+// Participation : To participate in any of our events you need to make a status post related to the ongoing event theme with a link to the event thread. Mention us in your post. After that you can claim any and all badges you like.
+// <img src="https://cdn.discordapp.com/attachments/1136989445992751264/1148145538617856070/asdasdaUntitledb-1.png" width="100%">
+// <h1><a target="_blank" rel="noopener noreferrer"><strong>Claim The Badges!</strong></a></h1>
+
+// <strong>Badges By  : <a href="/user/itszeeyah" target="_blank" rel="noopener noreferrer">@itszeeyah</a></strong>
+// <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160915186396450836/Gintoki_1.png?ex=65366585&amp;is=6523f085&amp;hm=c6577341e220d2d537060e034dad727b0a35c3e3c65c73839f76a164106b043f&amp;" width="240"> <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160915207711895593/Gintoki_2.png?ex=6536658a&amp;is=6523f08a&amp;hm=379bb1587b29d56d50eba1f6077fe413679c76e09438b86463f0c91f057f38bd&amp;" width="240"> <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160915228603715604/Gintoki_3.png?ex=6536658f&amp;is=6523f08f&amp;hm=2bde3e079fad6e62481b1ba512ead7ae8ccc64e672f0d182c6f5180ce79a317c&amp;" width="240">
+// <strong>Badges By : <a href="/user/Thunderblaze" target="_blank" rel="noopener noreferrer">@Thunderblaze</a></strong>
+// <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160456627510587473/Badge-1-10_10_23.png?ex=6534ba74&amp;is=65224574&amp;hm=97954d126012b3823e985a616430fc94184cd0beb6d2d35b847b86d85eb89d1c&amp;" width="240"> <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160456695517032538/Badge-2-10_10_23.png?ex=6534ba84&amp;is=65224584&amp;hm=44fdfbe2610fca94de4582129b7e1225a71c69d62f57cc81b07796bb63b687f5&amp;" width="240"> <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160456781642862642/Badge-3-10_10_23.png?ex=6534ba98&amp;is=65224598&amp;hm=c94909a73883ec5847269757167c5fedf71abc42d980ee8a338725bd3cdecb10&amp;" width="240">
+// <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160457028540579890/Badge-4-10_10_23.png?ex=6534bad3&amp;is=652245d3&amp;hm=63c3972a336dac6d38328043ebc8e01feb410001fb23121f508bf855ff60cdfc&amp;" width="240"> <img src="https://cdn.discordapp.com/attachments/1136989514653519924/1160457124103606394/Badge-5-10_10_23.png?ex=6534baea&amp;is=652245ea&amp;hm=75ad599d05334e7ad9b4f8469b2220750591e07a8e0745d1056490f746349bf4&amp;" width="240">
+// <img src="https://cdn.discordapp.com/attachments/1136989445992751264/1148145538617856070/asdasdaUntitledb-1.png" width="100%"><br><strong>Recruiting for free!</strong><br><strong>Join if not interested!</strong>
+// <img src="https://cdn.discordapp.com/attachments/1136989445992751264/1148145538617856070/asdasdaUntitledb-1.png" width="100%">
+// </center>
